da3ed0df8b622eca9f6f1e5744ea8b3e7dba310f
gabrielfst30 <gabrielfst30@gmail.com>
2024-02-19 23:44:41 -0300

    feature: revalidate para trocar imagens da api e estilos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,15 @@ import { GameProps } from "@/utils/types/game";
 import Link from "next/link";
 import Image from "next/image";
 
+import { BsArrowRightSquare } from "react-icons/bs";
+import { Input } from "@/components/input";
+
 //Chamando API
 async function getDalyGame() {
   try {
     const response = await fetch(
-      `${process.env.NEXT_API_URL}/next-api/?api=game_day`
+      `${process.env.NEXT_API_URL}/next-api/?api=game_day`,
+      { next: { revalidate: 60 } } //atualizando a req cada 60 segundos
     );
 
     return response.json();
@@ -27,16 +31,24 @@ export default async function Home() {
         </h1>
         <Link href={`/game/${dalyGame.id}`}>
           <section className="w-full bg-black rounded-lg">
-            <Image
-              src={dalyGame.image_url}
-              alt={dalyGame.title}
-              priority={true}
-              height={200}
-              width={200}
-              quality={100}
-            />
+            <div className="w-full max-h-96 h-96 relative rounded-lg">
+              <div className="absolute z-20 bottom-0 p-3 flex justify-center items-center gap-2">
+                <p className="font-bold text-xl text-white">{dalyGame.title}</p>
+                <BsArrowRightSquare size={24} color="#fff" />
+              </div>
+              <Image
+                src={dalyGame.image_url}
+                alt={dalyGame.title}
+                priority={true}
+                quality={100}
+                fill={true}
+                className="max-h-96 object-cover rounded-lg opacity-50 hover:opacity-100 transition-all duration-300"
+                sizes="(max-width: 768px) 100vw, (max-width:1200px) 33vw"
+              />
+            </div>
           </section>
         </Link>
+        <Input />
       </Container>
     </main>
   );
